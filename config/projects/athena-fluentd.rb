5cd11a4724359c99f686567d37afc275256a44ae
Masahiro Nakagawa <repeatedly@gmail.com>
2015-01-30 08:15:16 +0900

    Fix lzma undefined issue at plugin installation on CentOS 7

--- config/projects/athena-fluentd.rb
+++ config/projects/athena-fluentd.rb
@@ -16,6 +16,10 @@ dependency "preparation"
 
 override :zlib, :version => '1.2.8'
 override :rubygems, :version => '2.2.1'
+# CentOS7 needs latest liblzma to build pg and some gems
+if ohai['platform_family'] == 'rhel' && ohai['platform_version'].split('.').first.to_i == 7
+  override :liblzma, :version => '5.1.2alpha'
+end
 override :curl, :version => '7.28.0'
 
 # athena-fluentd dependencies/components
