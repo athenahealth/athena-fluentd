1a832634e4f9710b54ab7ef59e3b537b841a10ec
Stas Alekseev <stas.alekseev@gmail.com>
2015-02-04 13:36:32 -0500

    Switching to using curl 7.28.0, update Chef version for package build, v1.0.2.

--- config/projects/athena-fluentd.rb
+++ config/projects/athena-fluentd.rb
@@ -8,14 +8,15 @@ homepage "http://athenahealth.com"
 description "Athenahealth Fluentd and plugins"
 
 install_dir     "/opt/athena-fluentd"
-build_version   "1.0.1"
-build_iteration 1
+build_version   "1.0.2"
+build_iteration 0
 
 # creates required build directories
 dependency "preparation"
 
 override :zlib, :version => '1.2.8'
 override :rubygems, :version => '2.2.1'
+override :curl, :version => '7.28.0'
 
 # athena-fluentd dependencies/components
 dependency "athena-fluentd"
