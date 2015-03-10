e8cd7f5e1e18c1452f0e5b360d3a3c3acd0bdc44
Masahiro Nakagawa <repeatedly@gmail.com>
2015-03-10 19:21:20 +0900

    Add td-agent-cleanup software to remove unused files

--- config/projects/athena-fluentd.rb
+++ config/projects/athena-fluentd.rb
@@ -22,10 +22,19 @@ if ohai['platform_family'] == 'rhel' && ohai['platform_version'].split('.').firs
 end
 override :curl, :version => '7.28.0'
 
+<<<<<<< HEAD:config/projects/athena-fluentd.rb
 # athena-fluentd dependencies/components
 dependency "athena-fluentd"
 dependency "athena-fluentd-files"
 #dependency "athena-fluentd-ui"
+=======
+# td-agent dependencies/components
+dependency "td-agent"
+dependency "td-agent-files"
+dependency "td"
+dependency "td-agent-ui"
+dependency "td-agent-cleanup"
+>>>>>>> 07e94e8... Add td-agent-cleanup software to remove unused files:config/projects/td-agent2.rb
 
 # version manifest file
 dependency "version-manifest"
